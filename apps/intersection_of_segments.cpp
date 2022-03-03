c94a59cfcede5d1a94503567b9caac5940c5af02
Omkar Pitale <65304302+CyberKnight1803@users.noreply.github.com>
2022-03-03 18:57:55 +0530

    added functionalities - (#1)
    * added functionalities -
    1. _delete
    2. _delete_fixup
    3. erase
    4. _minimum
    5. _lower_bound
    6. _upper_bound
    
    Co-authored-by: Aditya Chopra <adeecc11@gmail.com>

--- apps/intersection_of_segments.cpp
+++ apps/intersection_of_segments.cpp
@@ -7,6 +7,13 @@ int main() {
     rb.insert(2, 2);
     rb.insert(3, 3);
     rb.insert(4, 4);
+    rb.insert(5, 5);
+    rb.insert(12, 12);
+    rb.insert(13, 13);
+    rb.insert(7, 7);
+    rb.insert(8, 8);
 
+    rb.erase(12);
+    
     return 0;
 }
--- include/ds/rb_tree.hpp
+++ include/ds/rb_tree.hpp
@@ -34,10 +34,16 @@ struct tree_t {
     void _left_rotate(node_ptr_t);
     void _right_rotate(node_ptr_t);
 
+    node_ptr_t _minimum(node_ptr_t);
+    node_ptr_t _upper_bound(K key);
+    node_ptr_t _lower_bound(K key);
+
+    void _transplant(node_ptr_t, node_ptr_t);
     void _delete(node_ptr_t);
     void _delete_fixup(node_ptr_t);
 
     void _insert_fixup(node_ptr_t);
+
 
    public:
     node_ptr_t insert(K key, V val);
@@ -172,13 +178,188 @@ void tree_t<K, V>::_insert_fixup(node_ptr_t z) {
 }
 
 template <typename K, typename V>
-void tree_t<K, V>::_delete(node_ptr_t) {}
+
+node_t<K, V> *tree_t<K, V>::_minimum(node_ptr_t x){
+    while(x->left != nil){
+        x = x->left;
+    }
+    return x;
+}
+
 
 template <typename K, typename V>
-void tree_t<K, V>::_delete_fixup(node_ptr_t) {}
+node_t<K, V> *tree_t<K, V>::_upper_bound(K key){
+    node_ptr_t x = root;
+    node_ptr_t upper_bound;
+
+    while(x != nil){
+        if (x->key == key){
+            upper_bound = x;
+            return upper_bound;
+        }
+
+        if (x->key < key){
+            x = x->right;
+        }
+        else {
+            upper_bound = x;
+            x = x->left;
+        }
+    }
+
+    return upper_bound;
+}
 
 template <typename K, typename V>
-void tree_t<K, V>::erase(K key) {}
+node_t<K, V> *tree_t<K, V>::_lower_bound(K key){
+    node_ptr_t x = root;
+    node_ptr_t lower_bound;
+
+    while(x != nil){
+        if (x->key == key){
+            lower_bound = x;
+            return lower_bound;
+        }
+
+        if (x->key < key){
+            lower_bound = x;
+            x = x->right;
+        }
+        else {
+            x = x->left;
+        }
+    }
+
+    return lower_bound;
+}
+
+template <typename K, typename V> 
+void tree_t<K, V>::_transplant(node_ptr_t u, node_ptr_t v){
+    if (u->p == nil)
+        root = v;
+    
+    else if (u == u->p->left)
+        u->p->left = v;
+    
+    else 
+        u->p->right = v;
+    
+    v->p = u->p;
+}
+
+
+template <typename K, typename V>
+void tree_t<K, V>::_delete(node_ptr_t z) {
+    node_ptr_t y = z;
+    Color y_original_color = y->color;
+
+    node_ptr_t x;
+    if (z->left == nil){
+        x = z->right;
+        _transplant(z, z->right);
+    }
+    else if (z->right == nil){
+        x = z->left;
+        _transplant(z, z->left);
+    }
+
+    else {
+        y = _minimum(z->right);
+        y_original_color = y->color;
+        x = y->right;
+
+        if (y->p == z){
+            x->p = y;
+        }
+        else {
+            _transplant(y, y->right);
+            y->right = z->right;
+            y->right->p = y;
+        }
+
+        _transplant(z, y);
+        y->left = z->left;
+        y->left->p = y;
+        y->color = z->color;
+    }
+
+    delete z;
+
+    if (y_original_color == Color::BLACK)
+        _delete_fixup(x);
+}
+
+template <typename K, typename V>
+void tree_t<K, V>::_delete_fixup(node_ptr_t x) {
+    node_ptr_t w;
+    while(x != root && x->color == Color::BLACK){
+        if (x == x->p->left){
+            w = x->p->right;
+            if (w->color == Color::RED){
+                w->color = Color::BLACK;
+                x->p->color = Color::RED;
+                _left_rotate(x->p);
+                w = x->p->right;
+            }
+
+            if (w->left->color == Color::BLACK && w->right->color == Color::BLACK){
+                w->color = Color::RED;
+                x = x->p;
+            }
+            else{ 
+                if (w->right->color == Color::BLACK){
+                    w->left->color = Color::BLACK;
+                    w->color = Color::RED;
+                    _right_rotate(w);
+                    w = x->p->right;
+                }
+                
+                w->color = x->p->color;
+                x->p->color = Color::BLACK;
+                w->right->color = Color::BLACK;
+                _left_rotate(x->p);
+                x = root;           
+            }
+        }
+        else {
+            w = x->p->left;
+            if (w->color == Color::RED){
+                w->color = Color::BLACK;
+                x->p->color = Color::RED;
+                _right_rotate(x->p);
+                w = x->p->left;
+            }
+
+            if (w->left->color == Color::BLACK && w->right->color == Color::BLACK){
+                w->color = Color::RED;
+                x = x->p;
+            }
+            else {
+                if (w->left->color == Color::BLACK){
+                    w->right->color = Color::BLACK;
+                    w->color = Color::RED;
+                    _left_rotate(w);
+                    w = x->p->left;
+                }
+
+                w->color = x->p->color;
+                x->p->color = Color::RED;
+                w->left->color = Color::RED;
+                _right_rotate(x->p);
+                x = root;
+            }
+        }
+    }
+
+    x->color = Color::BLACK;
+}
+
+
+template <typename K, typename V>
+void tree_t<K, V>::erase(K key) {
+    node_ptr_t dNode = _upper_bound(key);
+    _delete(dNode);
+}
 
 }  // namespace rb_tree
 }  // namespace DS
