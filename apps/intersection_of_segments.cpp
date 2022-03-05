b06cc9036b5c85b6d11c51aefc0ca232df00ba58
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-03-05 14:59:18 +0530

    fixed - 1. Lower bound 2. Upper bound
    Added -
    1. Search

--- apps/intersection_of_segments.cpp
+++ apps/intersection_of_segments.cpp
@@ -5,15 +5,25 @@ int main() {
     DS::rb_tree::tree_t<int, int> rb;
 
     rb.insert(2, 2);
-    rb.insert(3, 3);
-    rb.insert(4, 4);
-    rb.insert(5, 5);
-    rb.insert(12, 12);
-    rb.insert(13, 13);
     rb.insert(7, 7);
-    rb.insert(8, 8);
+    rb.insert(32, 32);
+    rb.insert(12, 12);
+    rb.insert(5, 5);
+    rb.insert(1, 1);
 
-    rb.erase(12);
-    
+    std::cout << "Lower Bounds: " << std::endl;
+    std::cout << rb._lower_bound(2)->key << std::endl;
+    std::cout << rb._lower_bound(7)->key << std::endl;
+    std::cout << rb._lower_bound(12)->key << std::endl;
+
+    std::cout << "Upper Bounds: " << std::endl;
+    std::cout << rb._upper_bound(7)->key << std::endl;
+    std::cout << rb._upper_bound(5)->key << std::endl;
+    std::cout << rb._upper_bound(12)->key << std::endl;
+
+    std::cout << "Search: " << std::endl;
+    std::cout << rb._search(12)->key << std::endl;
+
+    rb.erase(7);
     return 0;
 }
